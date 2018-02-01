5cd35e88bec96929fc7020600f2e8f317242113f
Therese Lindroth <31589557+thelindr@users.noreply.github.com>
2018-02-01 15:26:56 +0100

    Moved backend files to root

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(cors())
 // Connect to MongoDB, on the "products-api" database. If the db doesn't
 // exist, mongo will create it.
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/final-project"
+console.error(mongoUrl)
 mongoose.connect(mongoUrl, { useMongoClient: true })
 
 // This makes mongo use ES6 promises, instead of its own implementation
